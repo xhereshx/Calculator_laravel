ca6bc3aedd9cd3f5a23bb5d182b6a63e262ac52b
hazminfirdaus <cmhazminfirdaus@gmail.com>
2020-07-14 21:34:02 +0800

    login-register-styling

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name','surname', 'username', 'email', 'password',
+        'name', 'surname', 'username', 'email', 'password',
     ];
 
     /**
