b1e8209ac9265ff1ee665fe475722d58107061d1
Petr Jandal <jandalpetr@gmail.com>
2020-07-17 09:36:38 +0200

    pdf instalation library

--- config/app.php
+++ config/app.php
@@ -175,6 +175,12 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+          /*
+         * GitHub, laravel-dompdf....
+         */
+        Barryvdh\DomPDF\ServiceProvider::class,
+
+
     ],
 
     /*
@@ -226,6 +232,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'PDF' => Barryvdh\DomPDF\Facade::class,
 
     ],
 
